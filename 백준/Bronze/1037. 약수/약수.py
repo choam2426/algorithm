c8a2b1cf5c5fd842a75eda7490a5d19d190cf92e
choam2426 <choam2426@gmail.com>
2024-03-24 14:59:26 +0900

    update diretory

--- 백준/Bronze/1037. 약수/약수.py
+++
@@ -1,4 +0,0 @@
-N = int(input())
-divisor = list(map(int, input().split()))
-divisor.sort()
-print(divisor[0] * divisor[-1])
--- 백준/Bronze/10824. 네 수/네 수.py
+++
@@ -1,4 +0,0 @@
-A, B, C, D = input().split()
-
-result = int(A + B) + int(C + D)
-print(result)
