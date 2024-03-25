67fa69f71e32707fcfddf7beda566d529120fca5
choam2426 <choam2426@gmail.com>
2024-03-25 13:36:00 +0900

    BOJ 9935 solve

--- BOJ/gold/IV/9935 문자열 폭발/main.py
+++ BOJ/gold/IV/9935 문자열 폭발/main.py
@@ -2,29 +2,14 @@ import sys
 
 string = sys.stdin.readline().rstrip()
 bomb = sys.stdin.readline().rstrip()
+bomb_len = len(bomb)
 stack = []
-result = []
 for c in string:
-    try:
-        i = bomb.index(c)
-        if stack and i == stack[-1] + 1 or i == 0:
-            stack.append(i)
-            if stack[-1] == len(bomb) - 1:
-                for _ in range(len(bomb)):
-                    stack.pop()
-        else:
-            for j in stack:
-                result.append(bomb[j])
-            result.append(c)
-
-    except ValueError:
-        for j in stack:
-            result.append(bomb[j])
-        result.append(c)
-else:
-    for j in stack:
-        result.append(bomb[j])
-if result:
-    print("".join(result))
+    stack.append(c)
+    if len(stack) >= bomb_len and "".join(stack[-bomb_len:]) == bomb:
+        for _ in range(bomb_len):
+            stack.pop()
+if stack:
+    print("".join(stack))
 else:
     print("FRULA")
