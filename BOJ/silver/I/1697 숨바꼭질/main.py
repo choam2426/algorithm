a992c534830cfd12059adb569d568e7312f89899
choam2426 <choam2426@gmail.com>
2024-05-21 23:15:31 +0900

    1697 solve

--- BOJ/silver/I/1697 숨바꼭질/main.py
+++ BOJ/silver/I/1697 숨바꼭질/main.py
@@ -1,23 +1,23 @@
-x, goal = map(int, input().split())
+from collections import deque
 
-distance = goal - x
-time = 0
-if distance <= 0:
-    print(distance * -1)
-else:
-    while True:
-        tmp = x
-        x *= 2
-        distance = goal - x
-        if distance < 0:
-            if (distance * -1) >= goal - tmp:
-                time += goal - tmp
-                break
-            else:
-                time += distance * -1
-                break
-        elif distance == 0:
-            time += 1
-            break
-        time += 1
-    print(time)
+
+def move(pos):
+    yield pos - 1
+    yield pos + 1
+    yield pos * 2
+
+
+x, goal = map(int, input().split())
+dq = deque([(x, 0)])
+visit = [False] * 100001
+while True:
+    pos, time = dq.popleft()
+
+    if pos == goal:
+        print(time)
+        break
+
+    for dpos in move(pos):
+        if 0 <= dpos < 100001 and visit[dpos] == False:
+            dq.append((dpos, time + 1))
+            visit[dpos] = True
